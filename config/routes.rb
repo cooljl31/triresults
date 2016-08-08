94de075fb9f0f0dcbf78edb9a8b627ced561610e
Jim Lesperance <lesperancejim.lj@hotmail.com>
2016-08-08 17:53:51 +0000

    rspec spec/web_registration_spec.rb -e rq02

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+   resources :racers do
+    post "entries" => "racers#create_entry"
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'racers#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
